0c8dbcfdf88f8951d94965d12d1102b08b53480c
Ali Eren GUNER <alierenguner@hotmail.com>
2022-12-14 00:12:30 +0300

    Github folder has been created.

---
+++ blogtracer-client/src/components/comments/comment-title.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CommentTitle = () => {
+    return (
+        <h4>COMMENTS</h4>
+    )
+}
+
+export default CommentTitle;
--- blogtracer-client/src/components/comments/index.tsx
+++ blogtracer-client/src/components/comments/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import CommentCard from './comment-card';
 import CommentContainer from './comment-container';
 import CommentCreator from './comment-creator';
+import CommentTitle from './comment-title';
 
 const Comments = () => {
     return (
         <div className="comments">
-            <h4>COMMENTS</h4>
+            <CommentTitle />
             <CommentCreator />
             <CommentContainer>
                 <CommentCard />
